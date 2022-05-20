ee437c2c0f39a2ea5de94eecdd90bf88b28e4d85
Charles Hancock <hhancock@cnm.edu>
2022-05-20 11:46:29 -0600

    added user interface

---
+++ backend/src/utils/interfaces/User.ts
@@ -0,0 +1,16 @@
+export interface partialUser {
+    userId: string|null,
+    userUserName: string,
+    userEmail: string,
+    userHash: string,
+    userIsAdmin: boolean
+}
+
+export interface User {
+    userId: string|null,
+    userUserName: string,
+    userEmail: string,
+    userHash: string,
+    userIsAdmin: boolean
+
+}
