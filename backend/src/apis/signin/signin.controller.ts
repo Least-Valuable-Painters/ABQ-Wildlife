a06e2214d45ee0f9abb661196e8a4ac9362d5dc9
Mirjam Wolf <mwolf14@cnm.edu>
2022-05-20 11:57:55 -0600

    added signin.controller.ts.

---
+++ backend/src/apis/signin/signin.controller.ts
@@ -0,0 +1,74 @@
+import { NextFunction , Request, Response } from 'express'
+
+
+
+export async function signInController(request: Request, response: Response): Promise<Response | undefined> {
+
+    const {userEmail} = request.body
+    const mySqlResult: User | null = await selectUserByUserEmail(userEmail);
+    const isEmailValid: boolean = mySqlResult ? true : false
+
+    try {
+        const authenticate = async () => {
+
+            const {userPassword} = request.body;
+
+            // @ts-ignore isEmailValid determines mySqlResult will not be null
+            const {userId, userActivationToken, userEmail, userHash, userIsAdmin, userName} = mySqlResult
+
+            const user: User = {
+                userId,
+                userActivationToken,
+                userEmail,
+                userHash,
+                userIsAdmin,
+                userName
+            }
+
+            const signature: string = uuid();
+            const authorization: string = generateJwt({
+                userId,
+                userEmail,
+            }, signature);
+
+            const signInFailed = (message: string) => response.json({
+                status: 400,
+                data: null,
+                message
+            });
+
+            const signInSuccessful = () => {
+
+                // commented out for testing purposes
+                // if(profile.profileActivationToken !== null) {
+                // 	signInFailed("please activate your account")
+                // }
+
+                if (request.session) {
+                    request.session.user = user;
+                    request.session.jwt = authorization;
+                    request.session.signature = signature;
+                }
+                response.header({
+                    authorization
+                });
+
+                const isPasswordValid: boolean = user && await validatePassword(user.userHash, userPassword);
+
+                return isPasswordValid ? signInSuccessful() : signInFailed("Invalid email or password. Please try again.");
+            }
+
+            return isEmailValid ? authenticate() : response.json({
+                status: 400,
+                data: null,
+                message: "Invalid email or password."
+            })
+
+        }
+    catch
+        (error: any)
+        {
+            return response.json({status: 500, data: null, message: error.message})
+        }
+    }
+}
