de6b187d3f2705550d46a7138949f7475fd39753
Charles Hancock <hhancock@cnm.edu>
2022-05-20 10:50:39 -0600

    added signup.validator.ts

--- backend/src/apis/signup/signup.validator.ts
+++ backend/src/apis/signup/signup.validator.ts
@@ -5,7 +5,26 @@ export const signupValidator: Schema= {
         escape: true,
         trim: true,
         isLength: {
-            errorMessage: 'userAtHandle must be between'
+            errorMessage: 'userAtHandle must be between 1 and 128 characters',
+            options: {min:1, max: 32}
         }
+    },
+    userEmail: {
+        isEmail: {
+            errorMessage: "please provide a valid email address"
+        },
+        normalizeEmail: true,
+        trim: true
+    },
+    userPassword: {
+        isLength: {
+            errorMessage: 'Password must be at least 8 characters',
+            options: {min: 8}
+        },
+        trim: true,
+        escape: true
+    },
+    userIsAdmin: {
+
     }
 }
