e3c7d18674ca1de98d9ef15cc788e51042716d18
Mirjam Wolf <mwolf14@cnm.edu>
2022-05-26 10:32:29 -0600

    changes to file for location.

--- backend/src/utils/location/selectLocationByLocationId.ts
+++ backend/src/utils/location/selectLocationByLocationId.ts
@@ -1,15 +1,15 @@
-import {Location} from "../interfaces/Location";
 import {connect} from "../database.util";
 import {RowDataPacket} from "mysql2";
+import {Location} from "../interfaces/Location";
 
-export async function selectLocationByLocationId(locationId: string) : Promise<Location[]> {
+export async function selectLocationByLocationId(locationId: string): Promise<Location|null> {
     try {
-        const mySqlConnection = await connect();
-        const mySqlQuery = 'SELECT BIN_TO_UUID(locationId) as locationId, locationDateCreated, locationDescription, locationLat, locationLng, locationName'
-        const result = await <RowDataPacket>mySqlConnection.execute(mySqlQuery, {locationId})
-        return result[0] as Location[]
+        const mysqlConnection = await connect();
+        const sqlQuery: string = "SELECT BIN_TO_UUID(locationId) AS locationId, locationDateCreated, locationDescription, locationLat, locationLng, locationName FROM location WHERE locationId = UUID_TO_BIN(:locationId)"
+        const result = await mysqlConnection.execute(sqlQuery, {locationId}) as RowDataPacket[]
+        const location: Array<Location> = result[0] as Array<Location>
+        return location.length === 1 ? {...location[0]} : null;
     } catch (error) {
-        throw (error)
+        throw error
     }
-
 }
