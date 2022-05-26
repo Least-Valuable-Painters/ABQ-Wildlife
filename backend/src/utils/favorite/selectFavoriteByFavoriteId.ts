9c2283ecde93c42330a5f380c0bddab1aa86ccf4
Charles Hancock <hhancock@cnm.edu>
2022-05-26 10:11:43 -0600

    fixed minor syntax errors

--- backend/src/utils/favorite/selectFavoriteByFavoriteId.ts
+++ backend/src/utils/favorite/selectFavoriteByFavoriteId.ts
@@ -5,7 +5,7 @@ import {RowDataPacket} from "mysql2";
 export async function selectFavoriteByFavoriteId(favorite: Favorite): Promise<Favorite|null> {
     try {
         const mysqlConnection = await connect();
-        const mySqlSelectQuery = 'SELECT BIN_TO_UUID(favoriteUserId) as favoriteUserId, favoriteLocationId from `favorite` WHERE favoriteUserId = UUID_TO_BIN(:favoriteUserId)';
+        const mySqlSelectQuery = 'SELECT BIN_TO_UUID(favoriteLocationId) as favoriteLocationId, favoriteUserId from `favorite` WHERE favoriteUserId = UUID_TO_BIN(:favoriteUserId)';
         const result : RowDataPacket[]= await mysqlConnection.execute(mySqlSelectQuery, favorite) as RowDataPacket[]
         const rows: Favorite[] = result[0] as Favorite[]
         return rows.length !== 0 ? {...rows[0]} : null;
