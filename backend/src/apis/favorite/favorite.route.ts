4f35c4a8f00bfc734828b5c3f4fd548acbb09462
Charles Hancock <hhancock@cnm.edu>
2022-05-26 09:16:47 -0600

    fixed error

--- backend/src/apis/favorite/favorite.route.ts
+++ backend/src/apis/favorite/favorite.route.ts
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import {isLoggedIn} from "../../utils/controllers/isLoggedIn.controller";
 import {toggleFavorite} from "../../utils/favorite/toggleFavorite";
+import {toggleFavoriteController} from "./favorite.controller";
 
 const router = Router();
 
