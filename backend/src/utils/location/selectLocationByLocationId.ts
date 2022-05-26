46c3be867f5f261cc0f962a258567a0ae489bd7f
Mirjam Wolf <mwolf14@cnm.edu>
2022-05-26 10:20:41 -0600

    changed something.

--- backend/src/utils/location/selectLocationByLocationId.ts
+++ backend/src/utils/location/selectLocationByLocationId.ts
@@ -5,7 +5,7 @@ import {RowDataPacket} from "mysql2";
 export async function selectLocationByLocationId(locationId: string) : Promise<Location[]> {
     try {
         const mySqlConnection = await connect();
-        const mySqlQuery = 'SELECT BIN_TO_UUID(locationId) AS BIN_TO_UUID (locationId) AS locationId, locationDateCreated, locationDescription, locationLat, locationLng, locationName'
+        const mySqlQuery = 'SELECT BIN_TO_UUID(locationId) as locationId, locationDateCreated, locationDescription, locationLat, locationLng, locationName'
         const result = await <RowDataPacket>mySqlConnection.execute(mySqlQuery, {locationId})
         return result[0] as Location[]
     } catch (error) {
