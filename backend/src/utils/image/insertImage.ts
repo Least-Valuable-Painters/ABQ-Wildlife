d000ba4f0c710ed924906f2eb6a013c75fe9e3b9
Tyler Quintana <tquintana23@cnm.edu>
2022-05-27 10:36:05 -0600

    updated images files.

--- backend/src/utils/image/insertImage.ts
+++ backend/src/utils/image/insertImage.ts
@@ -4,7 +4,7 @@ import {Image} from "../interfaces/Image";
 export async function insertImage(image: Image) : Promise<string> {
     try {
         const mysqlConnection = await connect();
-        const query : string = 'INSERT INTO image(imageId, imageLocationId, imageUserId,  imageCloudinaryId,  imageDateCreated, imageUrl) VALUES (UUID_TO_BIN(UUID()), :imageUserId)';
+        const query : string = 'INSERT INTO image(imageId, imageLocationId, imageUserId,  imageCloudinaryId,  imageDateCreated, imageUrl) VALUES (UUID_TO_BIN(UUID()), :imageLocationId, :imageUserId, :imageCloudinaryId, :imageDateCreated, :imageUrl)';
         await mysqlConnection.execute(query, image);
         await mysqlConnection.end()
         return 'Image uploaded successfully.'
