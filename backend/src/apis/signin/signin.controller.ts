e32fa232d6435ff304ef11edfa837a3fd63417a9
Charles Hancock <hhancock@cnm.edu>
2022-05-31 16:53:06 -0600

    fixed signin files

--- backend/src/apis/signin/signin.controller.ts
+++ backend/src/apis/signin/signin.controller.ts
@@ -1,81 +1,46 @@
-import { NextFunction , Request, Response } from 'express'
-import {User} from "../../utils/interfaces/User";
+import { Request, Response } from 'express'
+import 'express-session'
+import { v4 as uuid } from 'uuid'
+import { generateJwt, validatePassword } from '../../utils/auth.utils'
+import { User } from '../../utils/interfaces/User'
 import {selectUserByUserEmail} from "../../utils/user/selectUserByUserEmail";
-import {generateJwt, validatePassword} from "../../utils/auth.utils";
-import uuid from "uuid";
 
 
-
-export async function signInController(request: Request, response: Response): Promise<Response | undefined> {
-
-    const {userEmail} = request.body
-    const mySqlResult: User | null = await selectUserByUserEmail(userEmail);
-    const isEmailValid: boolean = mySqlResult ? true : false
-
+export async function signInController (request: Request, response: Response): Promise<Response> {
     try {
-        const authenticate: () => Promise<any> = async () => {
+        const { UserEmail, UserPassword } = request.body
+        const User: User | null = await selectUserByUserEmail(UserEmail)
 
-            const {userPassword} = request.body;
-
-            // @ts-ignore isEmailValid determines mySqlResult will not be null
-            const {userId, userActivationToken, userEmail, userHash, userIsAdmin, userName} = mySqlResult
-
-            const user: User = {
-                userId,
-                userActivationToken,
-                userEmail,
-                userHash,
-                userIsAdmin,
-                userName
-            }
-
-            // @ts-ignore
-            const signature: string = uuid();
-            const authorization: string = generateJwt({
-                userId,
-                userEmail,
-            }, signature);
-
-            const signInFailed = (message: string) => response.json({
-                status: 400,
-                data: null,
-                message
-            });
-
-            const signInSuccessful = () => {
-
-                // commented out for testing purposes
-                // if(profile.profileActivationToken !== null) {
-                // 	signInFailed("please activate your account")
-                // }
-
-                if (request.session) {
-                    request.session.user = user;
-                    request.session.jwt = authorization;
-                    request.session.signature = signature;
-                }
-                response.header({
-                    authorization
-                });
-
-                return response.json({status: 200, data: null, message: "sign in successful"})
-            };
-
-            const isPasswordValid: boolean = user && await validatePassword(user.userHash, userPassword);
-
-                return isPasswordValid ? signInSuccessful() : signInFailed("Invalid email or password. Please try again.");
-            }
-
-            return isEmailValid ? authenticate() : response.json({
-                status: 400,
-                data: null,
-                message: "Invalid email or password."
-            })
-
-        }
-    catch
-        (error: any)
-        {
-            return response.json({status: 500, data: null, message: error.message})
-        }
+        return (User !== null) && await validatePassword(User.userHash, UserPassword)
+            ? signInSuccessful(request, response, User)
+            : signInFailed(response)
+    } catch (error: any) {
+        return response.json({ status: 500, data: null, message: error.message })
     }
+}
+
+function signInFailed (response: Response): Response {
+    return response.json({ status: 400, message: 'Email or password is incorrect please try again.', data: null })
+}
+
+function signInSuccessful (request: Request, response: Response, User: User): Response {
+    const { userId, userActivationToken, userEmail, userHash, userIsAdmin, userName } = User
+    const signature: string = uuid()
+    const authorization: string = generateJwt({
+        userId,
+        userActivationToken,
+        userEmail,
+        userHash,
+        userIsAdmin,
+        userName
+    }, signature)
+
+    request.session.user = User
+    request.session.jwt = authorization
+    request.session.signature = signature
+
+    response.header({
+        authorization
+    })
+    return response.json({ status: 200, message: 'Sign in successful', data: null })
+}
