f4b9bc227039409ab1a6d5b2b510cd2ce99f10c8
jdixon34 <102334927+jdixon34@users.noreply.github.com>
2022-06-04 06:40:57 -0600

    worked on navbar added second navbar to experiment

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Home } from './Home';
+import { Feed } from './Feed';
 import { FourOhFour } from './FourOhFour';
-import {OffCanvasNavbar} from "./Navbar";
+import {Navbar2} from "./Navbar2";
+// import {OffCanvasNavbar} from "./Navbar";
 
 
 
 export const App = () => (
     <>
         <BrowserRouter>
-            <OffCanvasNavbar/>
+            {/*<OffCanvasNavbar/>*/}
+            <Navbar2/>
             <Routes>
-                <Route  path='/' element={<Home />} />
+                <Route  path='/' element={<Feed />} />
                 <Route path='*' element={<FourOhFour />} />
             </Routes>
         </BrowserRouter>
---
+++ frontend/src/ui/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+
+
+export const Feed = () => {
+    return (
+        <>
+            <Container className="nav-open">
+            <div className="Body">
+                <p>i am the body</p>
+            </div>
+            </Container>
+        </>
+    );
+};
