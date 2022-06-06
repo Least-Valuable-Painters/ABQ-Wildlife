8cdfe121146967783402c87d300b49c41927b79d
jdixon34 <102334927+jdixon34@users.noreply.github.com>
2022-06-06 10:23:38 -0600

    fixed mistake

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -4,8 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Feed } from './Feed';
 import { FourOhFour } from './FourOhFour';
 import {OffCanvasNavbar} from "./Navbar";
-import { Upload } from './Upload'
-// import {OffCanvasNavbar} from "./Navbar";
 
 
 
@@ -13,10 +11,8 @@ export const App = () => (
     <>
         <BrowserRouter>
             <OffCanvasNavbar/>
-            {/*<Navbar2/>*/}
             <Routes>
-                <Route path='/' element={<Feed />} />
-                <Route path='/upload' element={<Upload />} />
+                <Route  path='/' element={<Feed />} />
                 <Route path='*' element={<FourOhFour />} />
             </Routes>
         </BrowserRouter>
