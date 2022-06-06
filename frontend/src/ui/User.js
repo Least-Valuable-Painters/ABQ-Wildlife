2065820f6530f9b37babbd7d83855cc7c433522e
Charles Hancock <hhancock@cnm.edu>
2022-06-06 09:35:49 -0600

    worked on form formatting

--- frontend/src/ui/User.js
+++ frontend/src/ui/User.js
@@ -1,8 +1,37 @@
 import React from "react";
+import {Button, Col, Container, Form, FormCheck, FormControl, FormGroup, FormLabel, Row} from "react-bootstrap";
 
 export function User () {
   return(
-    <p>user</p>
-
+    <>
+      <h1 className="text-center"><u>Profile</u></h1>
+    <Container>
+      <Row>
+        <Col>
+      <Form className="w-50 mx-auto">
+        <FormGroup>
+          <FormLabel>Name</FormLabel>
+          <FormControl type="name" placeholder="please enter your name"></FormControl>
+        </FormGroup>
+        <FormGroup>
+          <FormLabel>Email</FormLabel>
+          <FormControl type="email" placeholder="please enter your email address"></FormControl>
+        </FormGroup>
+        <FormGroup>
+          <FormLabel>Password</FormLabel>
+          <FormControl type="password" placeholder="please enter your password"></FormControl>
+        </FormGroup>
+        <FormGroup controlId="formBasicCheckbox">
+          <FormCheck type="checkbox" label="Turn location data on:" />
+        </FormGroup>
+        <FormGroup className="" controlId="formBasicCheckbox">
+          <FormCheck type="checkbox" label="Get email updates:" />
+        </FormGroup>
+        <Button variant="primary">Submit</Button>
+      </Form>
+        </Col>
+      </Row>
+    </Container>
+      </>
   )
 }
