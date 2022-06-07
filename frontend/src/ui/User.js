ef696115ba4dc5ac22e503b6c376532ee42f3452
Charles Hancock <hhancock@cnm.edu>
2022-06-07 10:28:52 -0600

    updated form formatting

--- frontend/src/ui/User.js
+++ frontend/src/ui/User.js
@@ -8,24 +8,24 @@ export function User () {
     <Container>
       <Row>
         <Col>
-      <Form className="w-50 mx-auto">
+      <Form className="w-50 mx-auto" id="contact" action="../../../backend/src/apis" method="post">
         <FormGroup className="mt-3">
-          <FormLabel>Name</FormLabel>
-          <FormControl type="name" placeholder="please enter your name"></FormControl>
+          <FormLabel>Username</FormLabel>
+          <FormControl type="username" placeholder="Please enter your username"></FormControl>
         </FormGroup>
         <FormGroup className="mt-3">
           <FormLabel>Email</FormLabel>
-          <FormControl type="email" placeholder="please enter your email address"></FormControl>
+          <FormControl type="email" placeholder="Please enter your email address"></FormControl>
         </FormGroup>
         <FormGroup className="mt-3">
           <FormLabel>Password</FormLabel>
-          <FormControl type="password" placeholder="please enter your password"></FormControl>
+          <FormControl type="password" placeholder="Please enter your password"></FormControl>
         </FormGroup>
         <FormGroup controlId="formBasicCheckbox">
-          <FormCheck className="mt-3" type="checkbox" label="Turn location data on:" />
+          <FormCheck className="mt-3" type="checkbox" label="Turn location data on" />
         </FormGroup>
         <FormGroup controlId="formBasicCheckbox">
-          <FormCheck className="mt-3" type="checkbox" label="Get email updates:" />
+          <FormCheck className="mt-3" type="checkbox" label="Get email updates" />
         </FormGroup>
         <Button className="mt-3" variant="primary">Submit</Button>
       </Form>
