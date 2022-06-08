ee059225171d8b5eba8bd03f2d8d380e8c19146a
jdixon34 <102334927+jdixon34@users.noreply.github.com>
2022-06-08 13:48:16 -0600

    organized import statements

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,27 +1,22 @@
-<<<<<<< HEAD
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import {Home} from './Home';
-import {Upload} from './Upload';
-import {FourOhFour} from './FourOhFour';
 import React from 'react';
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Home } from './Home'
-import { FourOhFour } from './FourOhFour'
-import React from 'react'
-import {Feed} from "./Feed"
->>>>>>> feed
-import {ClickFavorite} from "./Favorite";
-import {Navbar} from "./Navbar";
-import {ScratchMap} from "./ScratchMap";
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Provider} from "react-redux";
+
+import {Home} from './Home';
+import {FourOhFour} from './FourOhFour';
+
+
+import {Navbar} from "./Navbar";
+import {Upload} from './Upload';
+
+import {Feed} from "./Feed";
 import {User} from "./User";
+import {ClickFavorite} from "./Favorite";
+import {ScratchMap} from "./ScratchMap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 
-<<<<<<< HEAD
 
 export const App = (store) => (
     <>
@@ -32,25 +27,12 @@ export const App = (store) => (
                     <Route path='*' element={<FourOhFour/>}/>
                     <Route path='/favorite' element={<ClickFavorite/>}/>
                     <Route path='/favnav' element={<Navbar/>}/>
+                    <Route path='/feed' element={<Feed/>}/>
                     <Route path='/map' element={<ScratchMap/>}/>
                     <Route path='/upload' element={<Upload/>}/>
                     <Route path='/user' element={<User/>}/>
                 </Routes>
             </BrowserRouter>
         </Provider>
-=======
-export const App = () => (
-    <>
-        <BrowserRouter>
-            <Routes>
-                <Route  path='/home' element={<Home />} />
-                <Route path='/four' element={<FourOhFour />} />
-                <Route path='/favorite' element={<ClickFavorite />} />
-                <Route path='/favnav' element={<Navbar />} />
-                <Route path='/map' element={<ScratchMap />} />
-                <Route path='/feed' element={<Feed/>}/>
-            </Routes>
-        </BrowserRouter>
->>>>>>> feed
     </>
 )
