4845c839d58c6801c0dc840e084c05d89f3fbefe
Mirjam Wolf <mwolf14@cnm.edu>
2022-06-11 12:22:09 -0600

    started fixing some issues on the upload page.

--- frontend/src/ui/UploadImages/ImageUploader.js
+++ frontend/src/ui/UploadImages/ImageUploader.js
@@ -5,6 +5,7 @@ import {httpConfig} from "../shared/utils/httpConfig";
 import {values} from "lodash";
 import {Col, Container, Row} from "react-bootstrap";
 import {ImageDropZone} from "../shared/components/ImageDropZone";
+import {FormDebugger} from "../shared/components/FormDebugger";
 
 
 export function ImageUploader(props) {
@@ -12,107 +13,124 @@ export function ImageUploader(props) {
     console.log(locations)
     const validationObject = Yup.object().shape({
         imageLocationId: Yup.string()
-            .required('Please select a location')
+            .required('Please select a location').uuid('Please select a valid location'),
+        imageUrl: Yup.mixed().required("Images required")
+
     })
 
     function submitImage(value, {resetForm, setStatus}) {
 
-        const submitImage = (image) => {
-            httpConfig.post(`/apis/image/`, image)
-            .then(reply => {
-                let {message, type} = reply
-                if (reply.status === 200) {
-                    resetForm()
-                }
-                setStatus({message, type});
-                return (reply)
-            })
-        };
 
-        if (values.imageURL !== undefined) {
-            httpConfig.post(`/apis/image-upload/`, values.imageURL)
-                .then(reply => {
-                        let {message, type} = reply
-                        if (reply.status === 200) {
-                            submitImage({...values, imageUserId, imageURL: message})
-                        } else {
-                            setStatus({message, type});
-                        }
+        httpConfig.post(`/apis/image-upload/`, values.imageUrl)
+            .then(reply => {
+                    let {message, type} = reply
+                    if (reply.status === 200) {
+                        submitImage({...values, imageUserId, imageUrl: message})
+                    } else {
+                        setStatus({message, type});
                     }
-                );
+                }
+            );
+
+        function submitImage(image) {
+            httpConfig.post(`/apis/image/`, image)
+                .then(reply => {
+                    let {message, type} = reply
+                    if (reply.status === 200) {
+                        resetForm()
+                    }
+                    setStatus({message, type});
+                    return (reply)
+                })
         }
+
     }
 
+    const initialValues = {
+        imageLocationId: '',
+        imageUrl: '',
+
+    }
     return (
         <Formik
-            initialValues={''}
+            initialValues={initialValues}
             onSubmit={submitImage}
             validationSchema={validationObject}
         >
             {
                 (props) => {
-                const {
-                    setFieldValue,
-                    status,
-                    values,
-                    errors,
-                    touched,
-                    dirty,
-                    isSubmitting,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    handleReset,
-                    locations
-                } = props;
+                    const {
+                        setFieldValue,
+                        status,
+                        values,
+                        errors,
+                        touched,
+                        dirty,
+                        isSubmitting,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        handleReset,
+                    } = props;
 
-                return (
-                    <>
-                        <Container className="uploadBody">
-                            <Container>
-                                <Row>
-                                    <Col xs={0} md={1} lg={3}>
+                    return (
+                        <>
+                            <Container className="uploadBody">
+                                <Container>
+                                    <Row>
+                                        <Col xs={0} md={1} lg={3}>
 
-                                    </Col>
-                                    <Col xs={12} md={10} lg={6}>
-                                        <ImageDropZone
-                                            formikProps={{
-                                                values,
-                                                handleChange,
-                                                handleBlur,
-                                                setFieldValue,
-                                                fieldValue:"profileAvatarUrl"
-                                            }}
-                                        />
-                                        <form className="formContainer bg-primary rounded-3 w-100" action="./apis/" method="post" noValidate>
-                                            <Row className="m-3">
-                                                <Col lg={3} md={5} sm={6}>
-                                                    <label htmlFor="locationForm" className="labelItem"/>
-                                                    Location:
-                                                </Col>
-                                                <Col lg={9} md={7} sm={6}>
-                                                    {/*<input id="locationForm" type="select" name="locationForm"*/}
-                                                    {/*       placeholder="Location"*/}
-                                                    {/*       aria-label="locationForm"/>*/}
-                                                    <select name="imageLocationId" id="imageLocationId">
-                                                        {locations.map(location => <option value={location.locationId}>{location.locationName}</option>)}
-                                                    </select>
-                                                </Col>
-                                            </Row>
-                                        </form>
-                                        <div className="text-center m-3">
-                                            <button type="button" className="btn btn-primary">Upload Picture</button>
-                                        </div>
-                                    </Col>
-                                    <Col xs={0} md={1} lg={12}>
+                                        </Col>
+                                        <Col xs={12} md={10} lg={6}>
 
-                                    </Col>
-                                </Row>
+                                            <form className="formContainer bg-primary rounded-3 w-100" onSubmit={handleSubmit}>
+                                                <ImageDropZone
+                                                    formikProps={{
+                                                        values,
+                                                        handleChange,
+                                                        handleBlur,
+                                                        setFieldValue,
+                                                        fieldValue: "imageUrl"
+                                                    }}
+                                                />
+                                                <Row className="m-3">
+                                                    <Col lg={3} md={5} sm={6}>
+                                                        <label htmlFor="locationForm" className="labelItem"/>
+                                                        Location:
+                                                    </Col>
+                                                    <Col lg={9} md={7} sm={6}>
+                                                        {/*<input id="locationForm" type="select" name="locationForm"*/}
+                                                        {/*       placeholder="Location"*/}
+                                                        {/*       aria-label="locationForm"/>*/}
+                                                        <select name="imageLocationId" className="form-select" onBlur={handleBlur} onChange={handleChange} value={values.locationId}
+                                                                id="imageLocationId">
+                                                            {/*{console.log(locations)}*/}
+                                                            {locations.map(location => <option
+                                                                value={location.locationId}>{location.locationName}</option>)}
+                                                        </select>
+                                                    </Col>
+                                                </Row>
+                                                <div className="text-center m-3">
+                                                    <button type="submit" className="btn btn-primary">Upload Picture
+                                                    </button>
+                                                </div>
+
+
+
+
+                                            </form>
+                                            <FormDebugger {...props}/>
+
+                                        </Col>
+                                        <Col xs={0} md={1} lg={12}>
+
+                                        </Col>
+                                    </Row>
+                                </Container>
                             </Container>
-                        </Container>
-                    </>
-                )
-            }
+                        </>
+                    )
+                }
             }
         </Formik>
     )
@@ -152,29 +170,9 @@ export function ImageUploader(props) {
 //                                     fieldValue:"profileAvatarUrl"
 //                                 }}
 //                             />
-//                             <form className="formContainer bg-primary rounded-3 w-100" action="./apis/" method="post" noValidate>
-//                                 <Row className="m-3">
-//                                     <Col lg={3} md={5} sm={6}>
-//                                         <label htmlFor="locationForm" className="labelItem"/>
-//                                         Location:
-//                                     </Col>
-//                                     <Col lg={9} md={7} sm={6}>
-//                                         <input id="locationForm" type="text" name="locationForm"
-//                                                placeholder="Location"
-//                                                aria-label="locationForm"/>
-//                                     </Col>
-//                                 </Row>
-//                             </form>
-//                             <div className="text-center m-3">
-//                                 <button type="button" className="btn btn-primary">Upload Picture</button>
-//                             </div>
-//                         </Col>
-//                         <Col xs={0} md={1} lg={12}>
-//
-//                         </Col>
-//                     </Row>
-//                 </Container>
-//             </Container>
-//         </>
-//     )
-// };
+//                             <form className="formContainer bg-primary rounded-3 w-100" action="./apis/"
+// method="post" noValidate> <Row className="m-3"> <Col lg={3} md={5} sm={6}> <label htmlFor="locationForm"
+// className="labelItem"/> Location: </Col> <Col lg={9} md={7} sm={6}> <input id="locationForm" type="text"
+// name="locationForm" placeholder="Location" aria-label="locationForm"/> </Col> </Row> </form> <div
+// className="text-center m-3"> <button type="button" className="btn btn-primary">Upload Picture</button> </div> </Col>
+// <Col xs={0} md={1} lg={12}>  </Col> </Row> </Container> </Container> </> ) };
