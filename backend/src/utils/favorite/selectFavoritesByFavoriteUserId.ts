1c83ec4fe21e4731e2205289917e1bff197b35b4
Tyler Quintana <tquintana23@cnm.edu>
2022-06-15 10:38:29 -0600

    created selectFvoritesByFavoriteUserId

---
+++ backend/src/utils/favorite/selectFavoritesByFavoriteUserId.ts
@@ -0,0 +1,14 @@
+import {Favorite} from "../interfaces/Favorite";
+import {connect} from "../database.util";
+import {RowDataPacket} from "mysql2";
+
+export async function selectFavoritesByFavoriteUserId(favoriteUserId: string): Promise<Favorite[]> {
+    try {
+        const mysqlConnection = await connect();
+        const mySqlSelectQuery = 'SELECT BIN_TO_UUID(favoriteLocationId) as favoriteLocationId, favoriteUserId from `favorite` WHERE favoriteUserId = UUID_TO_BIN(:favoriteUserId)';
+        const result : RowDataPacket[]= await mysqlConnection.execute(mySqlSelectQuery, {favoriteUserId}) as RowDataPacket[]
+        return result[0] as Favorite[]
+    } catch(error) {
+        throw error
+    }
+}
